3643097fb62f01a6932512beeba76c4d40415ed1
wmliu <123@321>
2023-10-12 16:10:13 +0800

    feat(blog): add comment section

--- src/api/bocft/index.ts
+++ src/api/bocft/index.ts
@@ -16,7 +16,7 @@ export function sendMi6Broadcast(params?: object): Promise<ListResult> {
 
 export function sendS20Broadcast(params?: object): Promise<ListResult> {
   return http.request({
-    url: "/tailscale/http:////cell-s20:8080/api/broadcast",
+    url: "/tailscale/http:\\/\\/cell-s20:8080/api/broadcast",
     method: "get",
     params
   });
